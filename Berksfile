cc0274767f3247d8f9ba233a1067a1f093ae513b
pikesley <github@orgraphone.org>
2014-09-09 10:59:13 +0100

    Everything is terrible
    and nothing fucking works properly

--- Berksfile
+++ Berksfile
@@ -11,6 +11,12 @@ cookbook 'odi-ruby', github: 'theodi/chef-odi-ruby'
 cookbook 'rackspace-mysql-proxy', github: 'theodi/rackspace-mysql-proxy'
 cookbook 'odi-apt', github: 'theodi/odi-chef', rel: 'site-cookbooks/odi-apt'
 cookbook 'rbenv', github: 'fnichol/chef-rbenv'
+
 cookbook 'odi-deployer', github: 'theodi/chef-odi-deployer'
+
+cookbook 'odi-base', github: 'theodi/chef-odi-base'
+cookbook 'odi-monitoring', github: 'theodi/chef-odi-monitoring'
+cookbook 'serverdensity', github: 'serverdensity/chef-serverdensity'
+cookbook 'odi-pk', github: 'theodi/odi-chef', rel: 'site-cookbooks/odi-pk'
 
 cookbook 'mysql'
--- attributes/default.rb
+++ attributes/default.rb
@@ -5,7 +5,7 @@ default['database'] = 'certificates'
 default['deployment']['assets_allow_origin'] = '*'
 default['deployment']['precompile_assets'] = 'false'
 default['deployment']['rack_env'] = 'production'
-default['deployment']['revision'] = 'testicicle'
+default['deployment']['revision'] = 'STAGING'
 default['envbuilder']['base_dbi'] = 'production'
 default['envbuilder']['base_dir'] = '/home/certificates'
 default['envbuilder']['group'] = 'certificates'
@@ -19,3 +19,9 @@ default['rack_env'] = 'production'
 default['requires_memcached'] = 'true'
 default['user'] = 'certificates'
 default['odi-ruby']['users'] = {'certificates' => ["1.9.3-p547"]}
+
+default['chef_client']['cron']['use_cron_d'] = true
+default['chef_client']['cron']['hour'] = "*"
+default['chef_client']['cron']['minute'] = "*/5"
+default['chef_client']['cron']['log_file'] = "/var/log/chef/cron.log"
+default['chef_client']['server_url'] = 'https://chef.theodi.org'
